ec2f67070b296544e2ca6d0c30c446cb746ce1c8
Larry ~ Xiao Di <xiaodi@sjtu.edu.cn>
2013-11-26 16:38:33 +0800

    add copy to clipboard feature on toolbar icon (browser_action)
    copy the tab to clipboard
    	focus, select, execute chrome extension command ...
    TODO understand javascript execution sequence ( when the following is put outside for clause, can't find the object)
    EXP  use chrome developer tools to figure out
    BUG  the query method return highlighted tab of each window, not the current one

--- popup.js
+++ popup.js
@@ -1,10 +1,39 @@
+// [How to copy to clipboard in Chrome extensionPakzilla](http://wp.me/ptmKj-vb)
+function copyToClipboard( text ){
+        var copyDiv = document.createElement('div');
+        copyDiv.contentEditable = true;
+        document.body.appendChild(copyDiv);
+        copyDiv.innerHTML = text;
+        copyDiv.unselectable = "off";
+        copyDiv.focus();
+        document.execCommand('SelectAll');
+        document.execCommand("Copy", false, null);
+        document.body.removeChild(copyDiv);
+      }
+
+
 function showtabs () {
   var tabsDiv = document.getElementById("tabs");
   chrome.tabs.query({highlighted: true}, function expo (tab) {
     for (index = 0; index < tab.length; ++index) {
         console.log(tab[index]);
         temp = tab[index];
-        tabsDiv.innerHTML += "<p>["+temp.title+"]("+temp.url+")</p>";
+        // can't be hidden='true' to allow copy
+        tabsDiv.innerHTML += "<div id='text'>["+temp.title+"]("+temp.url+")</div>";
+        // copy the tab to clipboard
+        // TODO understand javascript execution sequence
+        // when the following is put outside for clause, can't find the object
+        // EXP  use chrome developer tools to figure out
+        // BUG  the query method return highlighted tab of each window, not the current one
+        //Get Input Element
+        // console.log(document.getElementById("text"));
+        tabsDiv.contentEditable = true;
+        tabsDiv.unselectable = "off";
+        tabsDiv.focus();
+        // document.getElementById("text").select();
+        //Copy Content
+        document.execCommand('SelectAll');
+        document.execCommand("Copy", false, null);
     }
   })
 }
